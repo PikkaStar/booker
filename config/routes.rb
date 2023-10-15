06935a7bf365c1f6bbd3366c236a84b6a1f883e5
PikkaStar <yokotaka1226@gmail.com>
2023-10-15 08:14:10 +0000

    follow

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
     resources :book_comments,only: [:create,:destroy]
   end
   devise_for :users
-   resources :users,only: [:index,:show,:edit,:update]
+   resources :users,only: [:index,:show,:edit,:update]do
+     resource :relationships,only: [:create,:destroy]
+     get 'followings'=>'relationships#followings',as: 'followings'
+     get 'followers'=>'relationships#followers',as: 'followers'
+   end
 end
